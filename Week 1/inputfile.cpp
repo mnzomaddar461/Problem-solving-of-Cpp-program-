33ccf1d86680a745d8fa7d25b62810771318a318
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2025-11-18 20:18:04 +0600

    Update

---
+++ Week 1/inputfile.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){
+    int id;
+    string name;
+    float cgpa;
+
+    cin >> id;
+    cin >> name;
+    cin >> cgpa;
+
+    std :: cout << "Student ID : " << id <<  std :: endl;
+    std :: cout << "Your name: " << name << std ::  endl;
+    std :: cout << "CGPA : " << cgpa << std :: endl;
+
+}
---
+++ Week 1/printfile.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include <string>
+int main(){
+    int id = 20;
+    std:: string name = "Naim Zomaddar";
+    float cgpa = 3.99;
+
+    std :: cout << "Student ID : " << id <<  std :: endl;
+    std :: cout << "Your name: " << name << std ::  endl;
+    std :: cout << "CGPA : " << cgpa << std :: endl;
+
+}
