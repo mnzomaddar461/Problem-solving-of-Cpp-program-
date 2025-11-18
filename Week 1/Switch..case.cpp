71ed47848395e7c3b87facbaee3977905683a95c
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2025-11-18 21:05:25 +0600

    Update

---
+++ Week 1/Switch..case.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int day;
+    cin >> day;
+
+    switch (day)
+    {
+    case 1 :
+        cout << "Sunday";
+        break;
+
+    case 2 :
+        cout << "Monday";
+        break;
+
+        case 3 :
+        cout << "Tuesday";
+        break;
+
+        case 4 :
+        cout << "Wednesday";
+        break;
+
+        case 5 :
+        cout << "Thursday";
+        break;
+
+        case 6 :
+        cout << "Friday";
+        break;
+
+        case 7 :
+        cout << "Saterday";
+        break;
+    
+    default:
+        cout << "Invalid Number!";
+        break;
+    }
+
+    return 0;
+}
