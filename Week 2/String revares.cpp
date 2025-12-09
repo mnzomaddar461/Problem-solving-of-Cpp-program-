611c42a733b391db3a3ec60a7ce37f9000fc6b05
mnzomaddar461 <mrnaimzomaddar461@gmail.com>
2025-12-09 20:00:02 +0600

    Update

---
+++ Week 2/String revares.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string value;
+    cin >> value;
+    reverse(value.begin(), value.end());
+    cout << value << endl;
+    return 0;
+}
